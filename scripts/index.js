d054e4c238e17298fce8f4418889f216a4b755cf
Nihal <nihalmujawar28413@gmail.com>
2024-07-11 12:57:17 +0530

    linked index.js using script tag

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,8 @@
 let input = document.querySelector("input");
 let button = document.querySelector("button");
+
+let hind = document.querySelector("hint");
+let count = document.querySelector("count");
+
+const randomNumber = Math.floor(Math.random() * 100 + 1);
+console.log(randomNumber);
