2448747fb239ff5d66c7bf26e1b1c9ecd0deb871
Nihal <nihalmujawar28413@gmail.com>
2024-07-11 13:23:07 +0530

    done

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,22 @@
 let input = document.querySelector("input");
 let button = document.querySelector("button");
 
-let hind = document.querySelector("hint");
-let count = document.querySelector("count");
+let hint = document.querySelector("#hint");
+let count = document.querySelector("#count");
+let counter = 0;
 
 const randomNumber = Math.floor(Math.random() * 100 + 1);
 console.log(randomNumber);
 
 button.addEventListener("click", () => {
-    
+  let inputValue = parseInt(input.value);
+  if (randomNumber === inputValue) {
+    hint.innerHTML = "Number is correct";
+  } else if (randomNumber < inputValue) {
+    hint.innerHTML = "input value is greater than random number";
+  } else {
+    hint.innerHTML = "input value is less than random number";
+  }
+  counter++;
+  count.innerHTML = `attempt ${counter}`;
 });
